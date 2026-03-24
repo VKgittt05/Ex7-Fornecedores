2db27580cca714c5d6da71abb45072885fdfa73b
Victor Kauã <victorelezeu@gmail.com>
2026-03-24 01:09:47 -0300

    Exercicio 7

---
+++ Fornecedores/src/test/java/com/Gerenciamento_Fornecedores/Fornecedores/FornecedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gerenciamento_Fornecedores.Fornecedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornecedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
